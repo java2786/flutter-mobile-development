36acd2cb2c9eb35037a10f89a7cbaaf3f535a2cc
Arun Kumar <java.kumar.arun@gmail.com>
2026-07-15 12:44:09 +0530

    Image

--- flutter_tutorials/multi_screen_app_5/lib/main.dart
+++ flutter_tutorials/multi_screen_app_5/lib/main.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-void main(){
-  runApp(MaterialApp(
-    title: "My Multi Screen App",
-    debugShowCheckedModeBanner: false,
-    // home: Text("Multi Pages"),
-    home: ScreenA(),
-  ));
+void main() {
+  runApp(
+    MaterialApp(
+      title: "My Multi Screen App",
+      debugShowCheckedModeBanner: false,
+      // home: Text("Multi Pages"),
+      home: ScreenA(),
+    ),
+  );
 }
 
-
-class ScreenA extends StatelessWidget{
-
-  void setValue() async{
+class ScreenA extends StatelessWidget {
+  void setValue() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.setString("name", "24.6");
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Screen A"),),
+      appBar: AppBar(title: Text("Screen A")),
       body: Center(
         child: ElevatedButton(
           onPressed: () {
@@ -34,32 +35,39 @@ class ScreenA extends StatelessWidget{
         ),
       ),
     );
-  } 
+  }
 }
 
-class ScreenB extends StatelessWidget{
-  
+class ScreenB extends StatelessWidget {
   String? hero = "Bat-man";
 
-  void readValue() async{
-    final SharedPreferences prefs = await SharedPreferences.getInstance();    
-      hero = prefs.getString("name");
-      print("Hero: $hero");
+  void readValue() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    hero = prefs.getString("name");
+    print("Hero: $hero");
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Screen B"),),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-          readValue();
-            // Navigator.pop(context); // goes back to the previous ScreenA
-          },
-          child: const Text("Go to Screen A"),
-        ),
+      appBar: AppBar(title: Text("Screen B")),
+      body: Column(
+        children: [
+ElevatedButton(
+              onPressed: () {
+                readValue();
+                Navigator.pop(context); // goes back to the previous ScreenA
+              },
+              child: const Text("Go to Screen A"),
+            ),
+       
+
+          Image.asset('assets/images/owl.jpg',
+          width: 200,
+          height: 120,
+          )
+        ],
       ),
     );
-  } 
+  }
 }
-
